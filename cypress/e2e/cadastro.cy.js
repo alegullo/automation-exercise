dec2fc156bab68bf30692087b652150412c1a345
alegullo <alexandre.gullo@hotmail.com>
2025-06-13 16:13:02 -0300

    config workflow

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,61 +1,79 @@
 /// <reference types="cypress" />
 
 describe('Cadastro de novo usuário', () => {
+  // Define dados do usuário em variáveis para fácil modificação
+  const userName = 'Alexandre QA';
+  const userEmail = `alexandre.qa+${Date.now()}@teste.com`; // Usa timestamp para email único
+  const userPassword = 'password1234';
+  const userDay = '1';
+  const userMonth = '1';
+  const userYear = '1990';
+  const userFirstName = 'Ale';
+  const userLastName = 'last test';
+  const userAddress = 'street 1';
+  const userCountry = 'United States'; // Valor que corresponde a uma opção no select
+  const userState = 'CA';
+  const userCity = 'Los Angeles';
+  const userZipcode = '0988789';
+  const userMobileNumber = '22980976';
+
+
   before(() => {
-    cy.visit('http://automationexercise.com')
-  })
+    cy.visit('http://automationexercise.com');
+  });
 
   it('Deve realizar cadastro e exclusão de usuário com sucesso', () => {
     // Navega até a página de Login/Signup
-    cy.get('a[href="/login"]').click()
+    cy.get('a[href="/login"]').click();
 
     // Verifica que a página de cadastro está visível
-    cy.url().should('eq','https://automationexercise.com/login')
+    cy.url().should('eq','https://automationexercise.com/login');
+    cy.contains('h2', 'New User Signup!').should('be.visible'); // Adicionando uma verificação de texto
 
-    // Preenche nome e email
-    cy.get('input[data-qa="signup-name"]').type('Alexandre QA')
-    cy.get('input[data-qa="signup-email"]').type('alexandre.qa@teste.com')
-    cy.get('button[data-qa="signup-button"]').click()
+    // Preenche nome e email usando comando customizado
+    cy.fillSignupForm(userName, userEmail);
 
     // Verifica que a tela de informações da conta está visível
-    cy.url().should('eq','https://automationexercise.com/signup')
+    cy.url().should('eq','https://automationexercise.com/signup');
+    cy.contains('b', 'Enter Account Information').should('be.visible'); // Adicionando uma verificação de texto
 
-    // Marca checkboxes
-    cy.get("#id_gender1").click();
-
-    // Preenche informações do cadastro
-    cy.get("[data-qa='password']").type("password1234");
-    cy.get('select[data-qa="days"]').select('1')
-    cy.get('select[data-qa="months"]').select('1')    
-    cy.get('select[data-qa="years"]').select('1990')
-
-    // Preenche informações obrigatorias
-    cy.get("[data-qa='first_name']").type("Ale");
-    cy.get("[data-qa='last_name']").type("last test");
-    cy.get("[data-qa='address']").type("street 1");
-    cy.get("[data-qa='country']").type("United States");
-    cy.get("[data-qa='state']").type("CA");
-    cy.get("[data-qa='city']").type("L");
-    cy.get("[data-qa='city']").type("Los A");
-    cy.get("[data-qa='city']").type("Los Angeles");
-    cy.get("[data-qa='zipcode']").type("0988789");
-    cy.get("[data-qa='mobile_number']").type("22980976");
+    // Preenche informações do cadastro usando comando customizado
+    cy.fillAccountInformationForm(
+      userPassword,
+      userDay,
+      userMonth,
+      userYear,
+      userFirstName,
+      userLastName,
+      userAddress,
+      userCountry,
+      userState,
+      userCity,
+      userZipcode,
+      userMobileNumber
+    );
 
     // Cria conta
-    cy.get('button[data-qa="create-account"]').click()
+    cy.get('button[data-qa="create-account"]').click();
 
     // Verifica que a conta foi criada
-    cy.url().should('eq','https://automationexercise.com/account_created')
-    cy.contains('b', 'Account Created!').should('be.visible')
-    cy.get('a[data-qa="continue-button"]').click()
+    cy.url().should('eq','https://automationexercise.com/account_created');
+    cy.contains('b', 'Account Created!').should('be.visible');
+    cy.get('a[data-qa="continue-button"]').click();
 
     // Confirma que o usuário está logado
-    cy.contains('b', 'Alexandre QA').should('be.visible')
+    // Pode ser necessário adicionar uma espera ou verificação mais robusta aqui
+    // dependendo de como a página carrega após o clique no continue
+    cy.url().should('eq', 'https://automationexercise.com/'); // Verifica se voltou para a home
+    cy.contains('b', userName).should('be.visible'); // Verifica o nome de usuário logado
 
     // Deleta conta
-    cy.get('a[href="/delete_account"]').click()
-    cy.url().should('eq','https://automationexercise.com/delete_account')
-    cy.contains('b', 'Account Deleted!').should('be.visible')
-    cy.get('[data-qa="continue-button"]').click()
-  })
-})
+    cy.get('a[href="/delete_account"]').click();
+    cy.url().should('eq','https://automationexercise.com/delete_account');
+    cy.contains('b', 'Account Deleted!').should('be.visible');
+    cy.get('[data-qa="continue-button"]').click();
+
+    // Opcional: Verificar se voltou para a home após deletar a conta
+    cy.url().should('eq', 'https://automationexercise.com/');
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -38,3 +38,29 @@ Cypress.Commands.add('login', (username, password) => {
 
     cy.get('button[data-qa="login-button"]').click()
 })
+
+// cypress/support/commands.js
+
+Cypress.Commands.add('fillSignupForm', (name, email) => {
+  cy.get('input[data-qa="signup-name"]').type(name);
+  cy.get('input[data-qa="signup-email"]').type(email);
+  cy.get('button[data-qa="signup-button"]').click();
+});
+
+Cypress.Commands.add('fillAccountInformationForm', (password, day, month, year, firstName, lastName, address, country, state, city, zipcode, mobileNumber) => {
+  cy.get("#id_gender1").click(); // Seleciona o gênero (assumindo masculino pelo id)
+  cy.get("[data-qa='password']").type(password);
+  cy.get('select[data-qa="days"]').select(day);
+  cy.get('select[data-qa="months"]').select(month);
+  cy.get('select[data-qa="years"]').select(year);
+  cy.get("[data-qa='first_name']").type(firstName);
+  cy.get("[data-qa='last_name']").type(lastName);
+  cy.get("[data-qa='address']").type(address);
+  cy.get("[data-qa='country']").select(country); // Usando select para país, pois é um dropdown
+  cy.get("[data-qa='state']").type(state);
+  cy.get("[data-qa='city']").type(city);
+  cy.get("[data-qa='zipcode']").type(zipcode);
+  cy.get("[data-qa='mobile_number']").type(mobileNumber);
+  // Checkboxes de newsletter e ofertas podem ser adicionados aqui se necessário
+});
+
